49afde3a2b103c95a15a7180ba952294fba9e319
Martin Cvitic <martincvitic19@gmail.com>
2023-11-16 22:47:12 +0100

    inc/: add dbus_client and dbus_server header files

---
+++ dbus_dispatcher/include/dbus_dispatcher/dbus_client.h
@@ -0,0 +1,29 @@
+/**
+* @file dbus_client.h
+*
+* @brief See the source file.
+* 
+* COPYRIGHT NOTICE: (c) 2022 Byte Lab Grupa d.o.o.
+* All rights reserved.
+*/
+
+#ifndef _DBUS_CLIENT_H_
+#define _DBUS_CLIENT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//--------------------------------- INCLUDES ----------------------------------
+
+//---------------------------------- MACROS -----------------------------------
+
+//-------------------------------- DATA TYPES ---------------------------------
+
+//---------------------- PUBLIC FUNCTION PROTOTYPES --------------------------
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _DBUS_CLIENT_H_
---
+++ dbus_dispatcher/include/dbus_dispatcher/dbus_server.h
@@ -0,0 +1,29 @@
+/**
+* @file dbus_server.h
+*
+* @brief See the source file.
+* 
+* COPYRIGHT NOTICE: (c) 2022 Byte Lab Grupa d.o.o.
+* All rights reserved.
+*/
+
+#ifndef _DBUS_SERVER_H_
+#define _DBUS_SERVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//--------------------------------- INCLUDES ----------------------------------
+
+//---------------------------------- MACROS -----------------------------------
+
+//-------------------------------- DATA TYPES ---------------------------------
+
+//---------------------- PUBLIC FUNCTION PROTOTYPES --------------------------
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _DBUS_SERVER_H_
