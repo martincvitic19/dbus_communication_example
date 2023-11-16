c8d3460f530929b3d103e60691e4cfe80d5103ae
Martin Cvitic <martincvitic19@gmail.com>
2023-11-16 22:48:21 +0100

    app/: add app_dbus_client and app_dbus_server "Hello, World!" application equivalents

---
+++ dbus_dispatcher/app/app_dbus_client.cpp
@@ -0,0 +1,33 @@
+/**
+* @file app_dbus_client.cpp
+*
+* @brief This app defines usage dbus_client's methods to implement sending
+         messages to a server.
+*
+* COPYRIGHT NOTICE: (c) 2023 AVL-AST d.o.o.
+* All rights reserved.
+*/
+
+//--------------------------------- INCLUDES ----------------------------------
+#include <stdint.h>
+#include <stdio.h>
+
+//---------------------------------- MACROS -----------------------------------
+
+//-------------------------------- DATA TYPES ---------------------------------
+
+//---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
+
+//------------------------- STATIC DATA & CONSTANTS ---------------------------
+
+//------------------------------- GLOBAL DATA ---------------------------------
+
+//------------------------------ PUBLIC FUNCTIONS -----------------------------
+
+//---------------------------- PRIVATE FUNCTIONS ------------------------------
+int main(void)
+{
+    printf("Hello from DBusClient app.\n");
+    return 0;
+}
+//---------------------------- INTERRUPT HANDLERS -----------------------------
