6fb5d6fcc8e87a7273c5502239a7481e2eda8838
Martin Cvitic <martincvitic19@gmail.com>
2023-11-20 18:07:35 +0100

    feat(dbus_server): implement message receiving functionality

--- dbus_dispatcher/app/app_dbus_server.cpp
+++ dbus_dispatcher/app/app_dbus_server.cpp
@@ -3,14 +3,13 @@
 *
 * @brief This app defines usage dbus_server's methods to implement receiving
          messages from a client.
-*
-* COPYRIGHT NOTICE: (c) 2023 AVL-AST d.o.o.
-* All rights reserved.
 */
 
 //--------------------------------- INCLUDES ----------------------------------
 #include <stdint.h>
 #include <stdio.h>
+#include <iostream>
+#include <sdbus-c++/sdbus-c++.h>
 
 //---------------------------------- MACROS -----------------------------------
 
@@ -21,13 +20,37 @@
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
 
 //------------------------------- GLOBAL DATA ---------------------------------
+sdbus::IObject* g_dbus_dispatcher{};
 
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
 
 //---------------------------- PRIVATE FUNCTIONS ------------------------------
-int main(void)
+void message_received(sdbus::MethodCall call)
+{
+    std::cout << "The test message was received. " << std::endl;
+}
+
+int main(int argc, char *argv[])
 {
     printf("Hello from DBusServer app.\n");
-    return 0;
+
+    // Create D-Bus connection to the session bus and request a name on it.
+    const char* serviceName = "org.sdbuscpp.dbus_dispatcher";
+    auto connection = sdbus::createSessionBusConnection(serviceName);
+
+    // Create a corresponding D-Bus object.
+    const char* objectPath = "/org/sdbuscpp/dbus_dispatcher";
+    auto message = sdbus::createObject(*connection, objectPath);
+
+    g_dbus_dispatcher = message.get();
+
+    // Register D-Bus methods and signals on the message object, and export the object.
+    const char* interfaceName = "org.sdbuscpp.DBusDispatcherInterface";
+    message->registerMethod(interfaceName, "dbus_dispatcher_method", "ais", "s", &message_received);
+    message->registerSignal(interfaceName, "dbus_dispatcher_signal", "s");
+    message->finishRegistration();
+
+    // Run the I/O event loop on the bus connection.
+    connection->enterEventLoop();
 }
 //---------------------------- INTERRUPT HANDLERS -----------------------------
