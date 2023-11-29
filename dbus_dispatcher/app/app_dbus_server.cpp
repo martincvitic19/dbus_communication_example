3ae0c3139907a7a96fdaab96b75a070b41a2250f
Martin Cvitic <martincvitic19@gmail.com>
2023-11-29 17:44:00 +0100

    feat(dbus_server): enable system connection to dbus (instead of session) and register an object's method on dbus

--- dbus_dispatcher/app/app_dbus_server.cpp
+++ dbus_dispatcher/app/app_dbus_server.cpp
@@ -21,7 +21,7 @@
  *        the method call message from the client.
  * @param call - the incoming method call message.
 */
-void notifyMessageArrivalCb(sdbus::MethodCall call);
+void notifyMessageArrival(sdbus::MethodCall call);
 
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
 
@@ -40,21 +40,20 @@ int main(int argc, char *argv[])
     // Establish D-Bus connection to the session bus and request a name on it.
     // This name will be used by D-Bus clients to find the service.
     const char* serviceName = "org.sdbuscpp.messageDispatcher";
-    auto connection = sdbus::createSessionBusConnection(serviceName);
+    auto connection = sdbus::createSystemBusConnection(serviceName);
 
     // Create a corresponding D-Bus object.
     const char* objectPath = "/org/sdbuscpp/messageDispatcher";
-    auto message = sdbus::createObject(*connection, objectPath);
+    auto message_parse = sdbus::createObject(*connection, objectPath);
 
-    g_nessage_dispatcher = message.get();
+    g_nessage_dispatcher = message_parse.get();
 
     // Register D-Bus methods and signals on the message object.
-    const char* interfaceName = "org.sdbuscpp.DBusDispatcherInterface";
-    message->registerMethod(interfaceName, "messageDispatcherMethod",
-                                             "ais", "s", &notifyMessageArrivalCb);
-    message->registerSignal(interfaceName, "messageDispatcherSignal", "s");
+    const char* interfaceName = "org.sdbuscpp.messageDispatcherInterface1";
+    message_parse->registerMethod(interfaceName, "notifyMessageArrival",
+                                             "s", "s", &notifyMessageArrival);
     // Export the object - this makes the object visible to the clients on the bus
-    message->finishRegistration();
+    message_parse->finishRegistration();
 
     // Run the I/O event loop on the bus connection - this handles all incoming,
     //  outgoing and other requests.
@@ -63,7 +62,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 //---------------------------- INTERRUPT HANDLERS -----------------------------
-void notifyMessageArrivalCb(sdbus::MethodCall call)
+void notifyMessageArrival(sdbus::MethodCall call)
 {
     //TODO: add error checking
 
@@ -75,10 +74,4 @@ void notifyMessageArrivalCb(sdbus::MethodCall call)
     // Since this is a void-returning method, just send an empty reply
     // back to the caller.
     reply.send();
-
-    // Emit the 'message_received' signal.
-    const char* interfaceName = "org.sdbuscpp.messagDispatcher";
-    auto signal = g_nessage_dispatcher->createSignal(interfaceName,
-                                         "notifyMessageArrivalCb");
-    g_nessage_dispatcher->emitSignal(signal);
 }
