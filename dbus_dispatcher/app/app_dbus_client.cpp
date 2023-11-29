44e8c593a68d4deb8b2642482f7a5aba8cbfd49b
Martin Cvitic <martincvitic19@gmail.com>
2023-11-29 17:42:25 +0100

    feat(dbus_client): implement a proxy to send a dummy message to the server via dbus

--- dbus_dispatcher/app/app_dbus_client.cpp
+++ dbus_dispatcher/app/app_dbus_client.cpp
@@ -3,14 +3,13 @@
 *
 * @brief This app defines usage dbus_client's methods to implement sending
          messages to a server.
-*
-* COPYRIGHT NOTICE: (c) 2023 AVL-AST d.o.o.
-* All rights reserved.
 */
 
 //--------------------------------- INCLUDES ----------------------------------
 #include <stdint.h>
 #include <stdio.h>
+#include <iostream>
+#include <sdbus-c++/sdbus-c++.h>
 
 //---------------------------------- MACROS -----------------------------------
 
@@ -28,6 +27,30 @@
 int main(void)
 {
     printf("Hello from DBusClient app.\n");
+
+    // Create a proxy object - view of the object from the client side
+    const char* destinationName = "org.sdbuscpp.messageDispatcher";
+    const char* objectPath = "/org/sdbuscpp/messageDispatcher";
+    auto testMessageProxy = sdbus::createProxy(destinationName, objectPath);
+
+    // Subscribing to the 'messageDispatcherSignal' signals
+    const char* interfaceName = "org.sdbuscpp.MessageDispatcherInterface1";
+    // testMessageProxy->registerSignalHandler(interfaceName, "messageDispatcherSignal", &onMessageSent);
+    testMessageProxy->finishRegistration();
+
+     std::string testMessage = "This is a test message.";
+
+    // Invoke sending a test message on given interface of the object
+    {
+        auto method = testMessageProxy->createMethodCall(interfaceName, "notifyMessageArrival");
+        method << testMessage;
+        auto reply = testMessageProxy->callMethod(method);
+        std::string result;
+        reply >> result;
+    }
+
+    sleep(1);
+
     return 0;
 }
 //---------------------------- INTERRUPT HANDLERS -----------------------------
